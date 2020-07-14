01c246671ed6dd27d128a33d996feedaadba29ad
Jozy Kinnaman <jozypants@gmail.com>
2020-07-13 19:26:16 -0700

    fixed api key.

--- src/components/EditTicketForm.js
+++ src/components/EditTicketForm.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
+import { useFirestore } from 'react-redux-firebase'
+import { firestore } from "firebase";
 
 function EditTicketForm (props) {
   const { ticket } = props;
 
+  function EditNewTicket(props) {
+    const firestore = useFirestore();
+  }
+
   function handleEditTicketFormSubmission(event) {
     event.preventDefault();
-    props.onEditTicket({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, timeOpen: ticket.timeOpen, formattedWaitTime: ticket.formattedWaitTime, id: ticket.id});
-  }
+    props.onEditTicket();
+    const propertiesToUpdate = {
+      names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value 
+      };
+  return firestore.update({collection: 'tickets', doc: ticket.id}, propertiesToUpdate)
+}
 
   return (
     <React.Fragment>
