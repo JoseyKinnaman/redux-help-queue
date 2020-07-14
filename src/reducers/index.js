b6c73f05a3534463318a3de25af1901a364b3a76
Jozy Kinnaman <jozypants@gmail.com>
2020-07-13 21:01:02 -0700

    add aut methods. Still buggy.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
 import {firestoreReducer} from 'redux-firestore'
 
+
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
   masterTicketList: ticketListReducer,
--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -2,19 +2,19 @@ import * as c from './../actions/ActionTypes';
 
 export default (state = {}, action) => {
 
-  // switch (action.type) {
+  switch (action.type) {
 
   // case c.DELETE_TICKET:
   //   const newState = { ...state };
   //   delete newState[id];
   //   return newState;
-  // case c.UPDATE_TIME:
-  //   const updatedTicket = Object.assign({}, state[id], {formattedWaitTime});
-  //   const updatedState = Object.assign({}, state, {
-  //     [id]: updatedTicket
-  //   });
+  // // case c.UPDATE_TIME:
+  // //   const updatedTicket = Object.assign({}, state[id], {formattedWaitTime});
+  // //   const updatedState = Object.assign({}, state, {
+  // //     [id]: updatedTicket
+  // //   });
   //   return updatedState;
-  // default:
+  default:
     return state;
-  // }
+  }
 };
